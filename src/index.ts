a8284f54d79c794572295160551b3f7f149ce59e
yashvendra-singh <yashvendra.singh29@gmail.com>
2022-07-09 13:07:42 +0530

    correcting project to run test and express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+// import express from 'express';
+
+// const app = express();
+// const port: number = 3000;
+
+// app.get('/students', (req, res) => {
+//     res.status(400).send('bad request');
+//   });
+
+// app.listen(port, (): void => {
+//     console.log(`Server started on localhost:${port}`);
+
+// });
+
 const myFunc = (num: number): number => {
     return num * num;
 };
